c43109afcf261730437fb88e62f847b2f3a612f6
Anna Vihrogonova <annavihrogonova@yahoo.com>
2018-01-28 20:57:16 +0000

    1)Create random color and assign it for background color and font color on click event. 2)Make fade out and fade in animation on click event

--- app.js
+++ app.js
@@ -1,38 +1,94 @@
-// Random Quoye
+/***********************************
+******** UI controller *************
+***********************************/
 const UICtrl = (function(){
 
+    //Random color 
+    function randomColor(){
+        let hex;
+
+        //Make hex color
+        hex = '#' + (function co(lor){   return (lor +=
+            [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+            && (lor.length == 6) ?  lor : co(lor); })('');
+       
+        return hex;
+    }
+            
+    /************ PUBLIC METHODS **********/
+
     return {
+
+        //Display quotes on UI
         show: function(data){
             let output = '';
            
             //Loop over data
             data.forEach(function(quote){
-                output += `<div>
-                                <p class="quotes">${quote.content}</p>
-                                <p class="author-name">${quote.title}</p>
-                            </div>`;
+                output += `<i class="fa fa-quote-left"></i>${quote.content}<p class="author-name">${quote.title}</p>`;
             });
 
-            // Display info
             document.querySelector('.random-quote').innerHTML = output;
+        },
+
+        //Display random color 
+        displayRandomColor: function(){
+            let hex , randomQuote, elements;
+
+            //Getting random color from private function
+            hex = randomColor();
+
+            //Getting elements from DOM
+            randomQuote = document.querySelector('.random-quote');
+            elements = document.querySelectorAll('body,' + '#wrapper,' + '.btn');
+
+            //Loop over elements, assign hex random
+            //color for background and font color
+            elements.forEach(function(element){
+               element.style.backgroundColor = hex;
+               element.style.transition = 'background 2s';
+               randomQuote.style.color = hex;
+            //    randomQuote.style.transition = 'color 3s';
+            });
+        },
+        //FadeOut and FadeIn
+        fadeOutAndFadeIn: function(element, classVisible, classHidden){
+            
+            if(element.classList.contains(classVisible)){
+                element.classList.remove(classVisible);
+                element.className += ' ' + classHidden;
+            }
+            
+            //Remove and add classes for fade out and fade in
+            setTimeout(function(){
+                element.classList.remove(classHidden);
+                element.className += ' ' + classVisible;
+                
+            },600);
         }
     }
 
 })();
 
 
+/***********************************
+******** APP controller *************
+***********************************/
 const AppCtrl = (function( UICtrl){
 
     // Function for eventListeners
     function evenetListeners() {
-        window.addEventListener('load', callApi);
-        document.querySelector('.btn-quote').addEventListener('click', callApi);
+        window.addEventListener('load', displayDataInfo);
+        document.querySelector('.btn-quote').addEventListener('click', displayDataInfo);
+        document.querySelector('.btn-quote').addEventListener('click', fadeInOut);
     }
 
-    // Getting response from API
-    function callApi(){
-        let url,bustCache;
 
+    function displayDataInfo(){
+        let url, bustCache, randomQuoteEl;
+        randomQuoteEl = document.querySelector('.random-quote');
+
+        // Getting response from API
         url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
         bustCache = '?' + new Date().getTime();
 
@@ -40,11 +96,25 @@ const AppCtrl = (function( UICtrl){
              .then(res => res.json())
              .then(function(data){
                 UICtrl.show(data);
+                console.log(data);
             })
             .catch(err => console.log(err));
+
+        //Random color
+        UICtrl.displayRandomColor();
+
     }
 
-    // Return event listeners function 
+    function fadeInOut(){
+        let randomQuote;
+        randomQuote = document.querySelector('.random-quote');
+        console.log(randomQuote);
+        //Fade out and fadeIn
+        UICtrl.fadeOutAndFadeIn( randomQuote, 'visible', 'hidden');
+   }
+
+
+    /*************** PUBLIC METHODS ****************/
     return {
        init: function() {
            return evenetListeners();
@@ -54,5 +124,5 @@ const AppCtrl = (function( UICtrl){
      
 })( UICtrl);
 
-
+//Invoke init function
 AppCtrl.init();
